908ea7e320cb1cc5c2ebe494e274c6a053011d34
Akira Kosuda <sayuen0@gmail.com>
2018-10-11 20:09:36 +0900

    initial commit

---
+++ Assets/Lesson1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lesson1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Debug.Log("Hello");
+        Person p = new Person();
+        p.firstName = "太郎";
+        p.lastName = "田中";
+        Debug.Log(p.GetFullName());
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
+
+class Person{
+    public string firstName;
+    public string lastName;
+
+    public string GetFullName (){
+        return this.lastName + this.firstName;
+    }
+}
+
+　
---
+++ Assets/Lesson2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class Lesson2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        List<int> list =new List<int>();
+        list.Add(48);
+        list.Add(985);
+        for (int i = 0; i < list.Count;i++){
+            Debug.Log(list[i]);
+        
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
